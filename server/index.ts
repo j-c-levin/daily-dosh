b05879ec7fc94a2c403bdbf6bd895bab9d06db00
Joshua Levin <joshua.c.levin@gmail.com>
2018-08-18 12:44:27 +0100

    begin work on the server auth endpoint

--- server/index.ts
+++ server/index.ts
@@ -10,19 +10,23 @@ app.get('/oauth/state', (_, res) => {
     res.send(v4());
 });
 
-app.get('/oauth/redirect', async (req) => {
-    console.log('got authorization token:', req.query.code);
+app.post('/auth', async (req, res) => {
+    console.log('got authorization token:', req.body.code);
     const monzoAuthUrl = 'https://api.monzo.com/oauth2/token';
-    const response = await post(monzoAuthUrl)
-        .send({
-            grant_type: 'authorization_code',
-            client_id: process.env.MONZO_CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            redirect_uri: process.env.REDIRECT_URI,
-            code: req.query.code,
-        })
-        .type('form');
-    console.log('got access token', response.body.access_token, 'got refresh token', response.body.refresh_token);
+    try {
+        const response = await post(monzoAuthUrl)
+            .type('form')
+            .send({
+                grant_type: 'authorization_code',
+                client_id: process.env.MONZO_CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                redirect_uri: process.env.REDIRECT_URI,
+                code: req.body.code,
+            });
+        res.send(response.body.access_token);
+    } catch (e) {
+        res.status(500).send('Error retrieving authorization code: ' + e);
+    }
 });
 
 app.listen(3000, () => console.log('Daily Dosh server running'));
