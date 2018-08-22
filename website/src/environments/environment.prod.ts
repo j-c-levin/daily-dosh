567504e2388daea0ab19f4519eabbac44cc4f8dd
Joshua Levin <joshua.c.levin@gmail.com>
2018-08-22 21:04:56 +0100

    auto remove the monzo key if it errors

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   clientId: 'oauth2client_00009ZPv5iE8z4yzVXu69B',
-  serverUrl: 'http://localhost:3000',
+  serverUrl: 'https://q9o5jlae92.execute-api.eu-west-1.amazonaws.com/latest',
   monzoStorageKey: 'MonzoStorageKey',
   startDateStorageKey: 'StartDateKey'
 };
