88adee01facad41bfd8322706a49b07d1c3baea7
Joshua Levin <joshua.c.levin@gmail.com>
2018-08-22 23:13:02 +0100

    wrong budget

--- website/src/app/store/state/monzo.state.ts
+++ website/src/app/store/state/monzo.state.ts
@@ -36,7 +36,7 @@ export class MonzoStateModel {
         accessKey: '',
         transactions: [],
         ignoredTransactions: [],
-        dailyBudget: 52,
+        dailyBudget: 36.8,
         startDay: '2018-07-26'
     }
 })
